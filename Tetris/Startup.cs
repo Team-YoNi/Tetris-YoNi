4ce023ba2680fa119012cc8350dae595685336eb
origami99 <origamista99@gmail.com>
2018-08-14 00:07:55 +0300

    Initialized the project

---
+++ Tetris/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                // Core game loop
+            }
+        }
+    }
+}
