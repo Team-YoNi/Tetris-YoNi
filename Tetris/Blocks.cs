3ac83d81c26267e5504af7f0d3e8a84d6883e4bb
yoanaborissova <yoana.borissova@gmail.com>
2018-08-14 00:46:00 +0300

    Added blocks :-)

--- Tetris/Blocks.cs
+++ Tetris/Blocks.cs
@@ -15,5 +15,89 @@
                 return this.firstBlock;
             }
         }
+
+        private readonly int[][] secondBlock =
+            {
+                new[] { 0, 0, 0, 0 },
+                new[] { 1, 1, 1, 1 }
+            };
+
+        public int[][] SecondBlock
+        {
+            get
+            {
+                return this.secondBlock;
+            }
+        }
+
+        private readonly int[][] thirdBlock =
+            {
+                new[] { 0, 0, 1 },
+                new[] { 1, 1, 1 }
+            };
+
+        public int[][] ThirdBlock
+        {
+            get
+            {
+                return this.thirdBlock;
+            }
+        }
+
+        private readonly int[][] fourthBlock =
+            {
+                new[] { 1, 1 },
+                new[] { 1, 1 }
+            };
+
+        public int[][] FourthBlock
+        {
+            get
+            {
+                return this.fourthBlock;
+            }
+        }
+
+        private readonly int[][] fifthBlock =
+            {
+                new[] { 0, 1, 1 },
+                new[] { 1, 1, 0 }
+            };
+
+        public int[][] FifthBlock
+        {
+            get
+            {
+                return this.fifthBlock;
+            }
+        }
+
+        private readonly int[][] sixthBlock =
+            {
+                new[] { 0, 1, 0 },
+                new[] { 1, 1, 1 }
+            };
+
+        public int[][] SixthBlock
+        {
+            get
+            {
+                return this.sixthBlock;
+            }
+        }
+
+        private readonly int[][] seventhBlock =
+            {
+                new[] { 1, 1, 0 },
+                new[] { 0, 1, 1 }
+            };
+
+        public int[][] SeventhBlock
+        {
+            get
+            {
+                return this.seventhBlock;
+            }
+        }
     }
 }
